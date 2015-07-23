6e49d69839bfe4bce14f4b072d3b14ae870d9373
Doh <tank2705@gmail.com>
2015-07-23 21:54:39 +0000

    add suthenticate_user!

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,7 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:edit, :update, :destroy]
   before_action :set_restaurant
+  before_action :authenticate_user!
   # GET /reviews
   # GET /reviews.json
 
