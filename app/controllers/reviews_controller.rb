da059b67936a6e6a354ea77248cc1b4f1d12e04f
Doh <tank2705@gmail.com>
2015-07-23 07:09:17 +0000

    Link review to restaurants

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:edit, :update, :destroy]
-
+  before_action :set_restaurant
   # GET /reviews
   # GET /reviews.json
 
@@ -18,6 +18,7 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.user_id=current_user.id
+    @review.restaurant_id = @restaurant.id
 
     respond_to do |format|
       if @review.save
@@ -58,6 +59,10 @@ class ReviewsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_review
       @review = Review.find(params[:id])
+    end
+    
+    def set_restaurant
+      @restaurant = Restaurant.find(params[:restaurant_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
