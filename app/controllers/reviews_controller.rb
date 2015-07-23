6ea727a16d4bd1cff44af5cb38dbb4c18c487539
Doh <tank2705@gmail.com>
2015-07-23 06:30:15 +0000

    review to User_Id

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,49 @@
+class ReviewsController < ApplicationController
+  before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+
+  respond_to :html
+
+  def index
+    @reviews = Review.all
+    respond_with(@reviews)
+  end
+
+  def show
+    respond_with(@review)
+  end
+
+  def new
+    @review = Review.new
+    respond_with(@review)
+  end
+
+  def edit
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.user_id=current_user.id
+    @review.save
+    respond_with(@review)
+  end
+
+  def update
+    @review.update(review_params)
+    respond_with(@review)
+  end
+
+  def destroy
+    @review.destroy
+    respond_with(@review)
+  end
+
+  private
+    def set_review
+      @review = Review.find(params[:id])
+    end
+
+    def review_params
+      params.require(:review).permit(:rating, :comment)
+    end
+end
