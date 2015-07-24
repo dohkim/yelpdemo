61c34c790dd04ffd6202b5befc780f51611fcb6d
Doh <tank2705@gmail.com>
2015-07-24 07:57:56 +0000

    Adding Validates

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,9 @@ class Restaurant < ActiveRecord::Base
   mount_uploader :image, ImageUploader
   has_many :reviews
   
+  validates :name, :address, :phone, :website, :image, presence: true
+  validates :phone, format: { with: /\A\(\d{3}\) \d{3}-\d{4}\z/,
+  message: "must be in the format (123) 456-7890" }
+  validates :website, format: { with: /\Ahttps?:\/\/.*\z/,
+  message: "must start with http:// or https://" }
 end
